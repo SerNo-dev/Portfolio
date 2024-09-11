59ead3a462084b65649523b099e2dc21fcac0eea
Sergio Nolasco <intell@hotmail.it>
2024-09-11 09:26:15 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { Route,RouterModule } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
+const routes : Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+  },{
+    path:'about',
+    component: AboutComponent
+  },{
+    path : 'services',
+    component :ServicesComponent
+  },{
+    path: 'portfolio',
+    component: PortfolioComponent
+  },{
+    path:'contact',
+    component:ContactComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
